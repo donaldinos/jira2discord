b4fb3fe2195fa83f034bd8c81eb7674630c8a90e
Donald <donaldinos@gmail.com>
2018-04-29 12:47:07 +0200

    test 5

--- index.js
+++ index.js
@@ -6,6 +6,114 @@ var http = require('http'),
 //
 var proxy = httpProxy.createProxyServer({});
 
+function parseBody(body, callback) {
+    let newBody
+    switch (origiBody.webhookEvent) {
+        case 'jira:issue_created':
+            newBody = {
+                "username": "Jira",
+                "avatar_url": "https://i.imgur.com/mdp3NY3.png",
+                "content": "Ticket byl vytvořen",
+                "embeds": [{
+                    "author": {
+                        "name": body.user.displayName,
+                        "icon_url": body.user.avatarUrls['48x48']
+                    },
+                    "title": body.issue.fields.description,
+                    "description": "[" + body.issue.key + ": " + body.issue.fields.summary + "](https://myocto.atlassian.net/browse/" + body.issue.key + ")",
+                    "color": 15351320,
+                    "fields": [{
+                            "name": "Typ ticketu:",
+                            "value": body.issue.fields.issuetype.name,
+                            "inline": true
+                        },
+                        {
+                            "name": "Priorita:",
+                            "value": body.issue.fields.priority.name,
+                            "inline": true
+                        }
+                    ]
+                }]
+            }
+            break;
+        case 'jira:issue_updated':
+            newBody = {
+                "username": "Jira",
+                "avatar_url": "https://i.imgur.com/mdp3NY3.png",
+                "content": "Ticket byl aktualizován",
+                "embeds": [{
+                    "author": {
+                        "name": body.user.displayName,
+                        "icon_url": body.user.avatarUrls['48x48']
+                    },
+                    // "title": body.issue.fields.description,
+                    "description": "[" + body.issue.key + ": " + body.issue.fields.summary + "](https://myocto.atlassian.net/browse/" + body.issue.key + ")",
+                    "color": 16249146,
+                    "fields": [{
+                            "name": "Typ ticketu:",
+                            "value": body.issue.fields.issuetype.name,
+                            "inline": true
+                        },
+                        {
+                            "name": "Priorita:",
+                            "value": body.issue.fields.priority.name,
+                            "inline": true
+                        }
+                    ]
+                }]
+            }
+            break;
+        case 'comment_created':
+            let comment
+            if (body.comment.body.length > 1000) {
+                comment = body.comment.body.substring(0, 1000) + "..."
+            } else {
+                comment = body.comment.body
+            }
+            newBody = {
+                "username": "Jira",
+                "avatar_url": "https://i.imgur.com/mdp3NY3.png",
+                "content": "Ticket byl komentován",
+                "embeds": [{
+                    "author": {
+                        "name": body.comment.author.displayName,
+                        "icon_url": body.comment.author.avatarUrls['48x48']
+                    },
+                    "title": body.issue.fields.description,
+                    "description": "[" + body.issue.key + ": " + body.issue.fields.summary + "](https://myocto.atlassian.net/browse/" + body.issue.key + ")",
+                    "color": 7465496,
+                    "fields": [{
+                            "name": "Typ ticketu:",
+                            "value": body.issue.fields.issuetype.name,
+                            "inline": true
+                        },
+                        {
+                            "name": "Priorita:",
+                            "value": body.issue.fields.priority.name,
+                            "inline": true
+                        },
+                        {
+                            "name": "Komentář:",
+                            "value": comment
+                        }
+                    ]
+                }]
+            }
+            break;
+        default:
+            newBody = {
+                "username": "Jira",
+                "avatar_url": "https://i.imgur.com/mdp3NY3.png",
+                "content": "!! Neošetřen stav: " + body.webhookEvent,
+                "embeds": [{
+                    // "title": body.issue.fields.description,
+                    "description": "[" + body.issue.key + ": " + body.issue.fields.summary + "](https://myocto.atlassian.net/browse/" + body.issue.key + ")",
+                    "color": 15258703
+                }]
+            }
+    }
+    callback(newBody)
+}
 //
 // Create your custom server and just call `proxy.web()` to proxy
 // a web request to the target passed in the options
@@ -20,10 +128,14 @@ var server = http.createServer(function(req, res) {
     });
     req.on('end', () => {
         res.end('ok');
-        proxy.web(req, res, { changeOrigin: true, target: 'https://discordapp.com/api/webhooks/439067758739587073/ha9l-06jomi48CxNVGz1r3up3V2ZZFPH-StZJ49x84Fkhokkqe7z_Wm4f8hznV9280qn' });
-        console.log("--------------")
-        console.log(body);
-        console.log("--------------")
+        JSON.parse(body);
+        parseBody(body, function(newBody) {
+            req.write(JSON.stringify(newBody));
+            proxy.web(req, res, { changeOrigin: true, target: 'https://discordapp.com/api/webhooks/439067758739587073/ha9l-06jomi48CxNVGz1r3up3V2ZZFPH-StZJ49x84Fkhokkqe7z_Wm4f8hznV9280qn' });
+            console.log("--------------")
+            console.log(newBody);
+            console.log("--------------")
+        })
     });
 });
 

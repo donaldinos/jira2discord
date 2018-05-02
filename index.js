acb2b1384a4fab0384b47c048070873f1007bd8b
Donald <donaldinos@gmail.com>
2018-05-02 20:09:08 +0200

    next fix version

--- index.js
+++ index.js
@@ -118,11 +118,10 @@ function parseBody(body, callback) {
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
     if (req.body) {
         console.log("1.1")
-        let bodyData = req.body;
-        // let bodyData = JSON.stringify(req.body);
+        let bodyData = JSON.stringify(req.body);
         // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
         proxyReq.setHeader('Content-Type', 'application/json');
-        // proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+        proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
         // stream the content
         console.log("1.2")
         proxyReq.write(bodyData);
